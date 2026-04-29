5fd26db745d304df6a1e23a3fce617e723163b68
MONISH-M-S <m.s.monish291207@gmail.com>
2026-04-29 13:01:50 +0530

    code changes due to change in prerequisite changes

--- SignalTransition.h
+++ SignalTransition.h
@@ -1,14 +1,4 @@
-#ifndef SIGNALTRANSITION_H
-#define SIGNALTRANSITION_H
-
-#include <iostream>
-using namespace std;
-
-class SignalTransition {
-public:
-    static void showYellow() {
-        cout << "YELLOW signal...\n";
-    }
-};
-
-#endif
+static void showYellow() {
+    cout << "\n--- SWITCHING SIGNAL ---\n";
+    cout << "YELLOW SIGNAL...\n";
+}
