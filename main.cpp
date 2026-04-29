b0bdd32010deb5920f6fde04c2e1d0772f5e9f68
MONISH-M-S <m.s.monish291207@gmail.com>
2026-04-29 13:02:49 +0530

    Implement user menu for traffic simulation final changes
    Added a menu for user interaction to start or exit the traffic simulation.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
+#include <iostream>
 #include "TrafficController.h"
+
+using namespace std;
 
 int main() {
     TrafficController t;
-    t.inputTraffic();
-    t.controlTraffic();
+    int choice;
+
+    do {
+        cout << "\n=====================================\n";
+        cout << "      SMART TRAFFIC SIMULATOR\n";
+        cout << "=====================================\n";
+        cout << "1. Start Simulation\n";
+        cout << "2. Exit\n";
+        cout << "Enter Choice: ";
+
+        cin >> choice;
+
+        if (choice == 1) {
+            t.inputTraffic();
+            t.controlTraffic();
+        }
+
+    } while (choice != 2);
+
     return 0;
 }
